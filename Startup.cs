3ddae0f0329dd16e4f425a98b98361848e306cb5
shj6214 <shj_1994@naver.com>
2021-07-15 22:45:01 +0900

    2rd

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace JenkinsWithDotnet
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello seunghwan22 ^^!");
+                    await context.Response.WriteAsync("Hello seunghwan22 ^^!!");
                 });
             });
         }
